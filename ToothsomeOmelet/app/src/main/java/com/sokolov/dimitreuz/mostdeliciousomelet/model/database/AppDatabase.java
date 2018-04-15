cb14f399f1087e3ec08d38ca9b174f81f1b970fa
dimitreuz <dimitreuzsokolov@gmail.com>
2018-04-15 13:20:21 +0300

    Added local database for storing received from api instances.

--- ToothsomeOmelet/app/src/main/java/com/sokolov/dimitreuz/mostdeliciousomelet/model/database/AppDatabase.java
+++ ToothsomeOmelet/app/src/main/java/com/sokolov/dimitreuz/mostdeliciousomelet/model/database/AppDatabase.java
@@ -1,7 +1,13 @@
 package com.sokolov.dimitreuz.mostdeliciousomelet.model.database;
 
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
+import com.sokolov.dimitreuz.mostdeliciousomelet.model.dto.OmeleteDB;
 
+
+@Database(entities = {OmeleteDB.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract OmeletDAO getOmeletDAO();
 }
