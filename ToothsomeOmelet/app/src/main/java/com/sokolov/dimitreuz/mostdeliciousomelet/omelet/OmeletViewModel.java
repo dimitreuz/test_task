2578e092b7f4537e37d37c3a5a4cacdab249d5a9
dimitreuz <dimitreuzsokolov@gmail.com>
2018-04-15 17:31:20 +0300

    Added view-model and ui skeletons for omelet model/

---
+++ ToothsomeOmelet/app/src/main/java/com/sokolov/dimitreuz/mostdeliciousomelet/omelet/OmeletViewModel.java
@@ -0,0 +1,4 @@
+package com.sokolov.dimitreuz.mostdeliciousomelet.omelet;
+
+public class OmeletViewModel {
+}
---
+++ ToothsomeOmelet/app/src/main/java/com/sokolov/dimitreuz/mostdeliciousomelet/ui/BaseActivity.java
@@ -0,0 +1,4 @@
+package com.sokolov.dimitreuz.mostdeliciousomelet.ui;
+
+public class BaseActivity {
+}
