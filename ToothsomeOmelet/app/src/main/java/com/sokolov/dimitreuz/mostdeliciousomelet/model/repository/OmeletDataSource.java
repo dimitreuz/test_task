77bd885a42737181d9e1c39ae17fbfe858a91c85
dimitreuz <dimitreuzsokolov@gmail.com>
2018-04-17 21:27:20 +0300

    Added java-docs to interfaces.

--- ToothsomeOmelet/app/src/main/java/com/sokolov/dimitreuz/mostdeliciousomelet/model/repository/OmeletDataSource.java
+++ ToothsomeOmelet/app/src/main/java/com/sokolov/dimitreuz/mostdeliciousomelet/model/repository/OmeletDataSource.java
@@ -7,6 +7,24 @@ import com.sokolov.dimitreuz.mostdeliciousomelet.model.DTO.Omelet;
 import java.util.List;
 import java.util.concurrent.Executor;
 
+/**
+ * Current interface described data source for
+ * omelet instance due to required context
+ * (remote storage, local database and so on).
+ *
+ * It describes repository pattern for omelet instance.
+ *
+ * @param <T> type of Omelet required by context.
+ * @see com.sokolov.dimitreuz.mostdeliciousomelet.model.repository.local.LocalOmeletDataSource
+ * @see com.sokolov.dimitreuz.mostdeliciousomelet.model.repository.remote.RemoteOmeletDataSource
+ *
+ * It provides methods to load instances from required
+ * source in asynchronious and synchronious ways.
+ *
+ * To make asynchronious request you should use
+ * @see ExecutionCallback
+ * to retrieve data from the source.
+ */
 public interface OmeletDataSource<T extends Omelet> {
 
     interface ExecutionCallback<T extends Omelet> {
